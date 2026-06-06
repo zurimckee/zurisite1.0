d3ba3243269ce170ae074c406d4caa94ba883b4b
Zuri McKee <zurimckee95@gmail.com>
2026-06-06 01:48:54 -0500

    lots of changes look at log entry 6.6.2026

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+
+function logContent(){
+    const logContent = document.querySelector('.log-content');
+    const entries = Array.from(logContent.querySelectorAll('p, hr'));
+    logContent.innerHTML = ''; // Clear current content
+    entries.reverse().forEach(entry => logContent.appendChild(entry)); // Append in reverse order
+}
+
 function toggleMenu(){
     const menu = document.querySelector(".menu-links");
     const icon = document.querySelector(".hamburger-icon");
@@ -5,3 +13,16 @@ function toggleMenu(){
     icon.classList.toggle("open");
 }
 
+function toggleMusic(){
+    const audio = document.getElementById("music");
+    const playpausebtn = document.getElementById("play-icon");
+    if (audio.muted) {
+        audio.muted = false;
+        playpausebtn.src = "assets/volup.png";
+    } else {
+        audio.muted = true;
+        playpausebtn.src = "assets/voldown.png";
+    }
+}
+
+logContent();
